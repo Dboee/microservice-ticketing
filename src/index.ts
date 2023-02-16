7c52cf819688b5fe57dd09440fc44a322c94f440
Dboee <Davidboee@gmail.com>
2023-02-16 08:38:25 +0100

    Changes db connection address to env variable

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,14 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+  // Check if MONGO_URI is defined
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
   // DB
   try {
-    await mongoose.connect('mongodb://tut-ticketing-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error(err);
