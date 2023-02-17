e46d0a20b34c92d69c1a82819fce5010e7b095e3
Dboee <Davidboee@gmail.com>
2023-02-17 10:25:06 +0100

    adds validation to update ticket route

--- src/routes/__test__/update.test.ts
+++ src/routes/__test__/update.test.ts
@@ -4,17 +4,27 @@ import { app } from '../../app';
 import mongoose from 'mongoose';
 
 import { Ticket } from '../../models/ticket';
+import { cookie } from 'express-validator';
 
-// const createTicket = (title: string, price: number) => {
-//   return request(app).post('/api/tickets').set('Cookie', global.signin()).send({
-//     title,
-//     price,
-//   });
-// };
+const createTicket = async (title: string, price: number, cookie: string[]) => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title,
+      price,
+    });
+  return response;
+};
 
 it('returns 404 if provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
-  await request(app).put(`/api/tickets/${id}`).send({}).expect(404);
+  const response = await request(app)
+    .put(`/api/tickets/${id}`)
+    .set('Cookie', global.signin())
+    .send({ title: 'Valid title', price: 20 });
+
+  expect(response.status).toEqual(404);
 });
 
 it('returns 401 if user is not authenticated', async () => {
@@ -23,54 +33,62 @@ it('returns 401 if user is not authenticated', async () => {
 });
 
 it('returns 401 if user does not own the ticket', async () => {
-  //   const response = await createTicket('Karpe: Amsterdam', 1500);
-  //   await request(app)
-  //     .put(`/api/tickets/${response.body.id}`)
-  //     .set('Cookie', global.signin())
-  //     .send({
-  //       title: 'Karpe: Oslo',
-  //       price: 2000,
-  //     })
-  //     .expect(401);
+  const response = await createTicket(
+    'Karpe: Amsterdam',
+    1500,
+    global.signin()
+  );
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: 'Karpe: Oslo',
+      price: 2500,
+    })
+    .expect(401);
+
+  expect(response.body.title).toEqual('Karpe: Amsterdam');
+  expect(response.body.price).toEqual(1500);
 });
 
 it('returns 400 if user provides an invalid title or price', async () => {
-  //   const cookie = global.signin();
-  //   const response = await createTicket('Karpe: Amsterdam', 1500);
-  //   await request(app)
-  //     .put(`/api/tickets/${response.body.id}`)
-  //     .set('Cookie', cookie)
-  //     .send({
-  //       title: '',
-  //       price: 2000,
-  //     })
-  //     .expect(400);
-  //   await request(app)
-  //     .put(`/api/tickets/${response.body.id}`)
-  //     .set('Cookie', cookie)
-  //     .send({
-  //       title: 'Karpe: Oslo',
-  //       price: -10,
-  //     })
-  //     .expect(400);
+  const cookie = global.signin();
+  const response = await createTicket('Karpe: Amsterdam', 1500, cookie);
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: 2000,
+    })
+    .expect(400);
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'Karpe: Oslo',
+      price: -10,
+    })
+    .expect(400);
 });
 
 it('updates the ticket provided valid inputs', async () => {
-  //   const cookie = global.signin();
-  //   const response = await createTicket('Karpe: Amsterdam', 1500);
-  //   await request(app)
-  //     .put(`/api/tickets/${response.body.id}`)
-  //     .set('Cookie', cookie)
-  //     .send({
-  //       title: 'Karpe: Oslo',
-  //       price: 2000,
-  //     })
-  //     .expect(200);
-  //   const ticketResponse = await request(app)
-  //     .get(`/api/tickets/${response.body.id}`)
-  //     .send();
-  //   expect(ticketResponse.body.title).toEqual('Karpe: Oslo');
-  //   expect(ticketResponse.body.price).toEqual(2000);
+  const cookie = global.signin();
+  const response = await createTicket('Valid title', 20, cookie);
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'New title',
+      price: 1000,
+    })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+  expect(ticketResponse.body.title).toEqual('New title');
+  expect(ticketResponse.body.price).toEqual(1000);
 });
 
 // it('publishes an event', async () => {
